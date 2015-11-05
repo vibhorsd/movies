183bc8d481e8125ff62cea5d72d9b312b25d04b9
Chanthu Nair <chanthu.nair@claricetechnologies.com>
2015-11-05 12:34:02 +0530

    sync get call to IMDB api

--- app/server.js
+++ app/server.js
@@ -1,16 +1,43 @@
 import express from "express";
 import React from "react";
 import ReactDOMServer from "react-dom/server";
+import request from "sync-request";
 import path from "path";
 import App from "./components/App";
 
 const app = express();
-
+const api_key = "541f4bed734234b7ec445338523c49fe";
 app.use(express.static(path.resolve(__dirname, "..", "dist")));
 app.get("/favicon.ico", (req, res) => res.send(""));
 
-app.use((req, res) => {
+var get_movies = function() {
     var movies = [];
+    var cur_year = new Date().getFullYear();
+    var movie_url = "http://api.themoviedb.org/3/discover/movie?primary_release_year=" + cur_year + "&api_key=" + api_key;
+    try {
+        var resp = request('GET', movie_url);
+        var movieBody = resp.body.toString('utf-8');
+        if(movieBody){
+            try {
+                
+                var movieData = JSON.parse(movieBody);
+                //console.log("movieData : " + JSON.stringify(movieData))
+                movies = movieData.results;
+                
+            } catch (err) {
+                console.log("PARSE ERR : " + err)
+            }
+        }
+        
+    } catch (err) {
+        console.log("GET ERR : " + err)
+    }
+    return movies
+}
+app.use((req, res) => {
+    var movies = get_movies();
+    console.log("Movies : " + JSON.stringify(movies))
+    
     var markup = "<!DOCTYPE html>";
     markup += "<html>";
     markup += "<head>";
@@ -25,6 +52,7 @@ app.use((req, res) => {
     markup += "<script src=\"bundle.js\"></script>";
     markup += "</body>";
     markup += "</html>";
+    
     res.send(markup);
 });
 
