50f7d19cd49b317611be6d71deeff457822ded49
Vibhor Deshmukh <vibhor.deshmukh@globant.com>
2015-11-19 17:53:06 +0530

    Some UI changes

--- app/stores/MovieStore.js
+++ app/stores/MovieStore.js
@@ -26,12 +26,12 @@ class MovieStore extends EventEmitter {
                 //this.emitChange();
                 break;
                 case AppConst.ActionTypes.MOVIE_SEARCH :
-                    var input = action.key;
-                    this.searchMovies(input);
-                    break;
+                var input = action.key;
+                this.searchMovies(input);
+                break;
                 case AppConst.ActionTypes.MOVIE_SEARCH_CLEAR:
-                    this.searchMovies("");
-                    break;
+                this.searchMovies("");
+                break;
                 default:
                 // no op
             }
@@ -39,13 +39,13 @@ class MovieStore extends EventEmitter {
         });
         
     }
-
+    
     set initial(obj){
         window.movies = obj.movies;
         this.totalPages = obj.totalPages;
         this.currentPage = obj.currentPage;
     }
-
+    
     update(pageNum){
         var self = this;
         console.log("ajax call to update")
@@ -59,7 +59,7 @@ class MovieStore extends EventEmitter {
                     window.movies.push(movieList[idx]);
                 }
                 console.dir(window.movies);
-                this.currentPage = pageNum;
+                self.currentPage = pageNum;
                 self.emitChange({showLoading: false});
             },
             error: function(xhr, status, err) {
@@ -72,37 +72,28 @@ class MovieStore extends EventEmitter {
         if (this.searchText) {
             var movies;
             movies = window.movies.filter((movie)=>{
-                return movie.title.match(new RegExp('^' + this.searchText.replace(/\W\s/g, ''), 'i'))})
-
-            if (movies.length > 0) {
-                return movies;
-            }
-            else {
-                return window.movies;
-            }
+                return movie.title.match(new RegExp('^' + this.searchText.replace(/\W\s/g, ''), 'i'))
+            });
+            return movies;
         }
         else
-            return window.movies;
+        return window.movies;
     }
     emitChange(change) {
-        
         var movies = this.getAllMovie();
         change.allMovies = movies;
-        if (this.searchText) {
-            change.search = true;
-        }
+        change.search = !!this.searchText;
         change.totalPages = this.totalPages;
         change.currentPage = this.currentPage;
-
         this.emit(CHANGE_EVENT, change);
     }
-
+    
     searchMovies(key){
         if (key.length > 0) {
             this.searchText = key;
         }
         else
-            this.searchText = null;
+        this.searchText = null;
         this.emitChange({showLoading:true});
     }
     /**
@@ -111,7 +102,7 @@ class MovieStore extends EventEmitter {
     addChangeListener(callback) {
         this.on(CHANGE_EVENT, callback);
     }
-
+    
     addSearchListner(callback){
         this.on(AppConst.StoreEvents.MOVIE_SEARCH, callback);
     }
